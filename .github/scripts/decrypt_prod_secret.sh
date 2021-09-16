d4363d992afda9cd5815b8be3bf46ff9f5d6b786
lucianodiamand <85711762+lucianodiamand@users.noreply.github.com>
2021-09-16 09:47:08 -0300

    Update decrypt_prod_secret.sh

--- .github/scripts/decrypt_prod_secret.sh
+++ .github/scripts/decrypt_prod_secret.sh
@@ -4,5 +4,5 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PROD" \
+gpg --quiet --batch --yes --decrypt --passphrase="$CLAVE_PROD" \
 --output $HOME/secrets/secret-prod.json secret-prod.json.gpg
