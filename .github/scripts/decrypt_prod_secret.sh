99ccc77beeff7f56a2491ecb433aa07cab0d8d62
Luciano Diamand <lucianodiamand@yahoo.com>
2021-10-21 08:37:09 -0300

    Eliminamos la extensión json

--- .github/scripts/decrypt_prod_secret.sh
+++ .github/scripts/decrypt_prod_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$CLAVE_PROD" \
---output $HOME/secrets/secret-prod.json secret-prod.json.gpg
+--output $HOME/secrets/secret-prod secret-prod.gpg
--- .github/scripts/decrypt_test_secret.sh
+++ .github/scripts/decrypt_test_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$CLAVE_TEST" \
---output $HOME/secrets/secret-test.json secret-test.json.gpg
+--output $HOME/secrets/secret-test secret-test.gpg
