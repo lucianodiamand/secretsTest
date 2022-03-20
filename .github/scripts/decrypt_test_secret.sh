e0de5b253b37d9b9264cb612bbfbd57ba1a52993
lucianodiamand <85711762+lucianodiamand@users.noreply.github.com>
2022-03-20 18:29:20 -0300

    Update decrypt_test_secret.sh

--- .github/scripts/decrypt_test_secret.sh
+++ .github/scripts/decrypt_test_secret.sh
@@ -4,5 +4,5 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$CLAVE_TEST" \
+gpg --quiet --batch --yes --decrypt --passphrase="$TEST_CLAVE" \
 --output $HOME/secrets/secret-test.json secret-test.json.gpg
